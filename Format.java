0b5255102ac2d643c43f6e500683cbfee37b21ee
HardProfy <BlackNight228@yandex.com>
2014-06-05 19:13:41 +0400

    Create Format.java

---
+++ Format.java
@@ -0,0 +1,184 @@
+
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class Format {
+	public static String main(Player player, String msg, String type) {
+		java.util.Date date = new java.util.Date(); 
+		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm:ss");
+		String time = sdf.format(date);
+		String format = null;
+
+		if (type.equalsIgnoreCase("global")) {
+			format = MemStorage.conf.getString("chat.global.format");
+		} else if (type.equalsIgnoreCase("private")) {
+			format = MemStorage.conf.getString("chat.private.format");
+		} else if (type.equalsIgnoreCase("socialspy")) {
+			format = MemStorage.conf.getString("chat.socialspy.format");
+		} else if (type.equalsIgnoreCase("map")) {
+			format = MemStorage.conf.getString("chat.map.format");
+		} else if (type.equalsIgnoreCase("local")) {
+			format = MemStorage.conf.getString("chat.local.format");
+		} else if (type.equalsIgnoreCase("emote")) {
+			format = MemStorage.conf.getString("chat.emote.format");
+		} else if (type.equalsIgnoreCase("admin")) {
+			format = MemStorage.conf.getString("chat.admin.format");
+		} else {
+			format = "";
+		}
+
+		format = format
+				.replace("%DISPLAYNAME%", player.getDisplayName())
+				.replace("%NAME%", player.getName())
+				.replace("%GROUPNAME%", LiveChat.chat.getPrimaryGroup(player))
+				.replace("%PREFIX%", LiveChat.chat.getPlayerPrefix(player))
+				.replace("%SUFFIX%", LiveChat.chat.getPlayerSuffix(player))
+				.replace("%WORLDNAME%", ((LiveChat)MemStorage.plugin).getMV().getAlias(player.getWorld()))
+				.replace("%GAMEMODE%", player.getGameMode().name())
+				.replace("%HEALTH%", Integer.toString(player.getHealth()))
+				.replace("%FOOD%", Integer.toString(player.getFoodLevel()))
+				.replace("%LEVEL%", Integer.toString(player.getLevel()))
+				.replace("%TOTALXP%", Integer.toString(player.getTotalExperience()))
+				.replace("%TIME%", time);
+
+		format = faction(format, player);
+		format = FormatTool.all(format);
+		format = format.replace("%MSG%", msg);
+
+		if (type.equalsIgnoreCase("global")) {
+			if (LiveChat.perms.has(player, "livechat.global.color")) {
+				format = FormatTool.color(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.global.format")) {
+				format = FormatTool.format(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.global.magic")) {
+				format = FormatTool.magic(format);
+			}
+		} else if (type.equalsIgnoreCase("private")) {
+			if (LiveChat.perms.has(player, "livechat.private.color")) {
+				format = FormatTool.color(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.private.format")) {
+				format = FormatTool.format(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.private.magic")) {
+				format = FormatTool.magic(format);
+			}
+		} else if (type.equalsIgnoreCase("map")) {
+			if (LiveChat.perms.has(player, "livechat.map.color")) {
+				format = FormatTool.color(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.map.format")) {
+				format = FormatTool.format(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.map.magic")) {
+				format = FormatTool.magic(format);
+			}
+		} else if (type.equalsIgnoreCase("local")) {
+			if (LiveChat.perms.has(player, "livechat.local.color")) {
+				format = FormatTool.color(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.local.format")) {
+				format = FormatTool.format(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.local.magic")) {
+				format = FormatTool.magic(format);
+			}
+		} else if (type.equalsIgnoreCase("emote")) {
+			if (LiveChat.perms.has(player, "livechat.emote.color")) {
+				format = FormatTool.color(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.emote.format")) {
+				format = FormatTool.format(format);
+			}
+			if (LiveChat.perms.has(player, "livechat.emote.magic")) {
+				format = FormatTool.magic(format);
+			}
+		} else if (type.equalsIgnoreCase("admin")) {
+			format = FormatTool.all(format);
+		} else if (type.equalsIgnoreCase("socialspy") && MemStorage.conf.getBoolean("chat.socialspy.color")) {
+			format = FormatTool.all(format);
+		}
+
+		return format;
+	}
+
+	public static String withTarget(Player sender, Player target, String format, String type) {
+		if (type.equalsIgnoreCase("private")) {
+			format = main(sender, format, "private");
+		} else if (type.equalsIgnoreCase("socialspy")) {
+			format = main(sender, format, "socialspy");
+		}
+
+		format = format
+				.replace("%TARGETDISPLAYNAME%", target.getDisplayName() + ChatColor.RESET)
+				.replace("%TARGETNAME%", target.getName());
+		return format;
+	}
+
+	public static String privateTarget(Player sender, Player target, String msg, String type) {
+		msg = 	Format.withTarget(sender, target, msg, "private")
+				.replaceFirst(target.getName(), MemStorage.locale.get("YOU"));
+		return msg;
+	}
+
+	public static String privateSender(Player sender, Player target, String msg, String type) {
+		msg = 	Format.withTarget(sender, target, msg, "private")
+				.replaceFirst(sender.getName(), MemStorage.locale.get("YOU"));
+		return msg;
+	}
+
+	public static String faction(String format, Player player) {
+		boolean NoFaction = false;
+		Plugin factions = Bukkit.getServer().getPluginManager().getPlugin("Factions");
+		if (factions != null) {
+			FPlayer fplayer = FPlayers.i.get(player);
+			if (fplayer != null) {
+				if (fplayer.hasFaction()) {
+					format = format.replace("%FACTIONTAG%", fplayer.getRole().getPrefix() + fplayer.getTag());
+					if (fplayer.getTitle().length() != 0) {
+						format = format.replace("%FACTIONTITLE%", fplayer.getRole().getPrefix() + fplayer.getTitle());
+					} else {
+						NoFaction = true;
+					}
+				} else {
+					NoFaction = true;
+				}
+			}
+		} else {
+			NoFaction = true;
+		}
+
+		if (NoFaction) {
+			format = format
+					.replace("%FACTIONTAG% ", "%FACTIONTAG%")
+					.replace("%FACTIONTAG%", "")
+					.replace("%FACTIONTITLE% ", "%FACTIONTITLE%")
+					.replace("%FACTIONTITLE%", "");
+		}
+
+		return format;
+	}
+
+	public static String userlist(Player player) {
+		String format = MemStorage.conf.getString("userlist.format");
+		format = FormatTool.all(format
+				.replace("%NAME%", player.getName())
+				.replace("%GROUPNAME%", LiveChat.chat.getPrimaryGroup(player))
+				.replace("%PREFIX%", LiveChat.chat.getPlayerPrefix(player))
+				.replace("%SUFFIX%", LiveChat.chat.getPlayerSuffix(player))
+				.replace("%HEALTH%", Integer.toString(player.getHealth()))
+				.replace("%FOOD%", Integer.toString(player.getFoodLevel()))
+				.replace("%LEVEL%", Integer.toString(player.getLevel()))
+				.replace("%TOTALXP%", Integer.toString(player.getTotalExperience())));
+		format = faction(format, player);
+
+		return format;
+	}
+}
